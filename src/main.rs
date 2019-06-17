701a045f294912777b4ddc266d5c012bc3808ac9
LAKSHITgoel <glakshit0@gmial.com>
2019-06-18 01:14:37 +0530

    done setting up rocket

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,43 @@
+#![feature(plugin, const_fn, proc_macro_hygiene, decl_macro)]
+
 #[macro_use]
 extern crate diesel;
 #[macro_use]
+extern crate r2d2;
+extern crate r2d2_diesel;
 extern crate dotenv;
+#[macro_use]
+extern crate rocket;
+extern crate rocket_contrib;
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
+extern crate serde_json;
 
 use diesel::pg::PgConnection;
 use crate::diesel::Connection;
 use dotenv::dotenv;
 use std::env;
+use rocket_codegen::routes;
 
 mod models;
 mod schema;
+mod db;
+mod static_files;
 
-fn main() {
+fn rocket() -> rocket::Rocket {
     dotenv().ok();
 
     let db_url = env::var("DATABASE_URL").expect("Set your Db URL");
-    let conn = PgConnection::establish(&db_url).unwrap();
 
-    let book = models::NewBook {
-        title: String::from("Gravity's rainbow"),
-        author: String::from("Thomas Pynchon"),
-        published: true,
-    };
+    let pool = db::init_pool(db_url);
+    rocket::ignite()
+        .manage(pool)
+        .mount("/",routes![static_files::all, static_files::index])
+}
 
-    if models::Book::insert_book(book, &conn) {
-        println!("Success");
-    } else {
-        println!("Failed");
-    }
+
+
+fn main() {
+    rocket().launch();
 }
---
+++ src/static_files.rs
@@ -0,0 +1,15 @@
+#![feature(custom_attribute, proc_macro_hygiene, decl_macro)]
+
+use std::io;
+use std::path::{Path, PathBuf};
+use rocket::response::NamedFile;
+
+#[get("/")]
+pub fn index() -> io::Result<NamedFile> {
+    NamedFile::open("public/index.html")
+}
+
+#[get("/<file..>", rank = 5)]
+pub fn all(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("public/").join(file)).ok()
+}
